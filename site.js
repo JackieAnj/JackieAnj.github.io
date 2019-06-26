4e98ca61cce3a76a0142fd1b7abd86003d92431f
Jackie <jackie.anj.liu@mail.utoronto.ca>
2019-06-25 22:28:48 -0400

    deleted some unused js functions

--- site.js
+++ site.js
@@ -1,43 +1,11 @@
-function openHome(cityName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-
-    // Show the specific tab content
-    document.getElementById(cityName).style.display = "block";
-
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-}
-
-function scroll(num) {
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        if (i != num) {
-            tablinks[i].style.backgroundColor = "";
-        } else {
-            tablinks[i].style.backgroundColor = black;
-        }
-    }
-}
-
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function () {
     var navbar = document.getElementById("menubar");
-    myFunction(navbar);
+    setSticky(navbar);
 };
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction(navbar) {
+function setSticky(navbar) {
     // Get the navbar
 
     if (window.pageYOffset >= 146) {
@@ -58,6 +26,15 @@ function closeNav() {
 }
 
 var tabcontent = document.getElementsByClassName("tablink");
+
+$(document).on('scroll', function() {
+    if($(this).scrollTop()<$('#about-me').position().top){
+        tabcontent[0].style.backgroundColor = "#333C42";
+        tabcontent[1].style.backgroundColor = "#333C42";
+        tabcontent[2].style.backgroundColor = "#333C42";
+        tabcontent[3].style.backgroundColor = "#333C42";
+    }
+})
 
 $(document).on('scroll', function() {
     if($(this).scrollTop()>=$('#about-me').position().top){
